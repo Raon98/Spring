ce5445b55ba6ded247e9680576e3d3deb8e6866e
Raon98 <cheol1998@kakao.com>
2022-06-03 16:14:55 +0900

    encoding

---
+++ src/main/java/com/example/encodingex/EncodingExApplication.java
@@ -0,0 +1,17 @@
+package com.example.encodingex;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class EncodingExApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncodingExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/encodingex/EncodingExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.encodingex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncodingExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
